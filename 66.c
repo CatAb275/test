b360dc38f837c0686aec2c205cce953446429716
CatAb275 <128575045+CatAb275@users.noreply.github.com>
2023-03-22 14:46:33 +0800

    added a file

---
+++ 66.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int intput=0;
+printf("good study?<1,0>");
+scanf("%d\n",&intput);
+if(intput==1)
+printf("good offer");
+else
+printf("sell hongshu");
+return 0;	 
+}
